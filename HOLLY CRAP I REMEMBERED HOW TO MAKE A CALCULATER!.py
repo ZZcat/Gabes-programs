7db71a13680cfa3b37cee53f43d8fd71e35bbcde
ZZcat <ZZcat@users.noreply.github.com>
2015-02-28 15:21:34 -0600

    Update to give errors

--- HOLLY CRAP I REMEMBERED HOW TO MAKE A CALCULATER!.py
+++ HOLLY CRAP I REMEMBERED HOW TO MAKE A CALCULATER!.py
@@ -1,4 +1,7 @@
-x = input("Number 1:")
-y = input("Number 2:")
-sum = x + y
-print x , "+" , y , "=" , sum
+try:
+  x = input("Number 1:")
+  y = input("Number 2:")
+  sum = x + y
+  print x , "+" , y , "=" , sum
+except:
+  print"You must enter a number or die"
