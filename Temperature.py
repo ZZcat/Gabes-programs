a257ad1f2c6fded7938282e963b40aa75416083f
ZZcat <zach@zettervall.com>
2015-03-01 13:55:27 -0600

    New file

---
+++ Temperature.py
@@ -0,0 +1,7 @@
+temp = input("What is the temperature?: ")
+if temp > 100: # this says if temp is more then 100 to the next tab lines Note if this is ran the following elif and elses will NOT run
+        print "You should probably stay inside today"
+elif temp > 70: # this is almost exactly like the the if but it will only run if the if is not true
+        print "Were shorts"
+elif temp < 70: 
+        print "were long pants"
